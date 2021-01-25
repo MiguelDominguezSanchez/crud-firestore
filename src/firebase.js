e27a4723d3ef08ce69bbf329e3d45ea822d03637
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-01-25 08:35:50 +0100

    56. Configurar Firebase en React

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore'
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyAXhZnsCSEvuQlpIAhFAxQBxrCcf1N4Up8',
+	authDomain: 'crud-react-a6738.firebaseapp.com',
+	projectId: 'crud-react-a6738',
+	storageBucket: 'crud-react-a6738.appspot.com',
+	messagingSenderId: '409049499677',
+	appId: '1:409049499677:web:1012d9f44e51ca221b1ef4',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export (firebase)
